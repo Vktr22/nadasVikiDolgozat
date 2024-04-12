6b2782d94e4c68f5644d290e0c163402e2d01f2b
Vktr22 <nds.viktoria@gmail.com>
2024-04-12 09:31:31 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Nádas Viktória</title>
+</head>
+<body>
+    <?php
+    /* 
+    Készítesz 3 tömböt, mely sofőr, és város neveket (tetszőleges tartalommal), illetve
+    véletlen számokat tartalmaz 1 és 200 között (zárt intervallum), és írasd ki a nevek mellé a városneveket
+    és a véletlen számokat, mint a tömegközlekedési eszköz (itt busz) sorszámát (elég 5 sor)! (3 pont)
+    
+    A kiíratásokat táblázatban jelenítsd meg: első oszlop: Név, második: Város neve, harmadik: Busz sorszáma.
+    A megjelenítés ciklussal történjen! Pl. így: (3 pont)
+    */
+    
+    ?>
+</body>
+</html>
